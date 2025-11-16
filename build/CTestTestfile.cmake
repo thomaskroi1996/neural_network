50e9a8b438a2f281d2d8f97ca19ae72094b83093
Thomas Kroißenbrunner <thomaskroi1996@gmail.com>
2025-11-16 14:31:48 +0100

    delete build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thk/coding/neural_network
-# Build directory: /home/thk/coding/neural_network/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(myNN_RunTests "/home/thk/coding/neural_network/build/myNN_tests")
-set_tests_properties(myNN_RunTests PROPERTIES  _BACKTRACE_TRIPLES "/home/thk/coding/neural_network/CMakeLists.txt;41;add_test;/home/thk/coding/neural_network/CMakeLists.txt;0;")
